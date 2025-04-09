95cb21a0820959fdf1c438cdb86e0ea094558459
Harpreet287 <124236553+Harpreet287@users.noreply.github.com>
2025-04-09 15:29:19 +0530

    Bootstrap commit

---
+++ src/main/java/com/trekon/trekon/TrekonApplication.java
@@ -0,0 +1,14 @@
+package com.trekon.trekon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekonApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/trekon/trekon/TrekonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trekon.trekon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
